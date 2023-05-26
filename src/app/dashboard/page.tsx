5fddce7c773fef1d23effaf14f1e8e1b695924f0
zachuri <punsalang.zachary@gmail.com>
2023-05-26 05:05:01 -0700

    supabase-provider: example to accessing session and supabase client from provider

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,15 @@
-import React from "react"
+'use client'
 
+import React from "react"
+import { useSupabase } from "@/components/providers/supabase-provider"
 
 export default function DashboardPage() {
+  const { supabase, session } = useSupabase()
+
   return (
     <div>
       <h1>DashBoard</h1>
+      <p>Email: {session?.user.email}</p>
     </div>
   )
 }
