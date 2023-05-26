a56c2344fb30a11f306630b1740bb6aff1a1fec5
zachuri <punsalang.zachary@gmail.com>
2023-05-26 01:59:28 -0700

    callback: added auth callback for next -> documentation from supabase

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers"
+import { NextResponse, type NextRequest } from "next/server"
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get("code")
+
+  if (code) {
+    const supabase = createRouteHandlerClient({ cookies })
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  // URL to redirect to after sign in process completes
+  return NextResponse.redirect(requestUrl.origin)
+}
