1b12b8cd76acbe46dec52155235d9d8233bb99c0
zachuri <punsalang.zachary@gmail.com>
2023-05-26 05:14:35 -0700

    dashboard: added sign out button example

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,29 @@
-'use client'
+"use client"
 
 import React from "react"
+import { useRouter } from "next/navigation"
+
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
 import { useSupabase } from "@/components/providers/supabase-provider"
 
 export default function DashboardPage() {
+  const router = useRouter()
+
   const { supabase, session } = useSupabase()
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.push("/")
+  }
 
   return (
     <div>
       <h1>DashBoard</h1>
       <p>Email: {session?.user.email}</p>
+      <button className={cn(buttonVariants())} onClick={handleSignOut}>
+        Sign out
+      </button>
     </div>
   )
 }
